af06693e427eb35e275873854aeb7ecb82c1a907
Namir7 <nalbert2012@ya.ru>
2021-10-17 18:58:17 +0300

    temp: temp commit

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const serveIndex = require("serve-index");
-const app = express();
 
+const app = express();
 const port = process.env.PORT;
-const static = process.env.STATIC_PATH
+const static = process.env.STATIC_PATH;
 
 app.use("/data", express.static(static));
-app.use('/data', serveIndex(static));
+app.use("/data", serveIndex(static));
 
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+  res.send("Hello world!");
 });
 
 app.listen(port, () => {
-    console.log(`server runnning at port ${port}`);
+  console.log(`server runnning at port ${port}`);
 });
